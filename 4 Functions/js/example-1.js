e02bb7a34e611d15aa74d91a0a953f06499e33f6
ViacheslavZuberu <clavik15@gmail.com>
2020-03-09 19:52:57 +0200

    Added function examples

---
+++ 4 Functions/js/example-1.js
@@ -0,0 +1,19 @@
+function invDigits(k) {
+    let result = 0;
+
+    while (k >= 1) {
+        let lastDigit = k % 10;
+        k -= lastDigit;
+        k /= 10;
+        result *= 10;
+        result += lastDigit;
+    }
+
+    return result;
+}
+
+console.log(invDigits(123));
+console.log(invDigits(50));
+console.log(invDigits(73));
+console.log(invDigits(0));
+console.log(invDigits(11223344));
